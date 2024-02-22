3dc29ca75de2249a379a22a670bdf50c4b6685dc
AwesomeJuneKim <158990924+AwesomeJuneKim@users.noreply.github.com>
2024-02-22 13:00:06 +0900

    함수 작성방법2

--- 03/03.js
+++ 03/03.js
@@ -1,10 +1,15 @@
-function handleClick(n){
+// 함수작성 1 (펑션 키워드 사용)
+// function handleClick(n){
     //메세지 영역 가져오기1
     //document.getElementById("msgArea").innerHTML="안녕하세요";
 
     //메세지 영역 가져오기2
     // document.querySelector("#msgArea").innerHTML = "<h2>버튼"+n+"이 눌러졌습니다.</h2>";
 
-    document.querySelector("#msgArea").innerHTML = `<h2>버튼 ${n}이 눌러졌습니다.</h2>`;
+    // document.querySelector("#msgArea").innerHTML = `<h2>버튼 ${n}이 눌러졌습니다.</h2>`;
 
+// }
+// 함수 작성 2(화살표 함수) 상수 const 변수 let
+const handleClick=(n)=>{
+    document.querySelector("#msgArea").innerHTML = "<h2>버튼"+n+"이 눌러졌습니다.</h2>";
 }
