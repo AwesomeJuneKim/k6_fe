20032cda24ce10b6155ff6f64c14257f85cf137e
AwesomeJuneKim <158990924+AwesomeJuneKim@users.noreply.github.com>
2024-02-22 12:45:10 +0900

    자바스프링 실습

--- 03/03.js
+++ 03/03.js
@@ -1,1 +1,8 @@
-// alert("안녕하세요")
+function handleClick(){
+    //메세지 영역 가져오기1
+    //document.getElementById("msgArea").innerHTML="안녕하세요";
+
+    //메세지 영역 가져오기2
+    document.querySelector("#msgArea").innerHTML = "<h2>안녕하세요</h2>";
+
+}
